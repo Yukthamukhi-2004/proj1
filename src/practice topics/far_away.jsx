5257fffcd87374f7e9212c52e3385585fb0fe31f
Yuktha <yukthakodumuru@gmail.com>
2026-01-28 18:46:26 +0530

    Add accordion, flashcards, text expander, far away app

---
+++ src/practice topics/far_away.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "./styles/far_away.css";
+
+function App() {
+  const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Charger", quantity: 1, packed: true },
+  ];
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList initialItems={initialItems} />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="header">
+      <h1>🌴Far Away🧳</h1>
+    </div>
+  );
+}
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    if (!description) return;
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do u need for ur trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item....."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList({ initialItems }) {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <span> you have x items on your list, and u already packed x </span>
+    </footer>
+  );
+}
+export default App;
